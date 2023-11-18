f813c5639da82b04ad6cd7b0703379f11726c739
Eugene Kosukhin <kosuha606@gmail.com>
2023-11-18 11:33:44 +0300

    feat(easy): array first

--- easy/array-first.ts
+++ easy/array-first.ts
@@ -1,4 +1,5 @@
 
+type First2<T extends any[]> = T extends [infer P, ...any[]] ? P : never
 type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
 type Length<T extends any[]> = T["length"] extends 0 ? 'zero' : 'morezero'
 type empty = First<[]>
