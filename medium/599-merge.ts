2f08ac5a3245dae88ef21ceefc9baa142f3f62df
Eugene Kosukhin <kosuha606@gmail.com>
2023-12-21 08:21:50 +0300

    feat: 599 merge

---
+++ medium/599-merge.ts
@@ -0,0 +1,19 @@
+
+type Merge<F, S> = {
+    [K in keyof F | keyof S]: K extends keyof S
+      ? S[K]
+      : K extends keyof F
+      ? F[K]
+      : never;
+  };
+
+  type Foo = {
+    a: number
+    b: string
+  }
+  type Bar = {
+    b: number
+    c: boolean
+  }
+
+  type one = Merge<Foo, Bar>
