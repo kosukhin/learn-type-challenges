aad3aa80ae01dbe54641051d96de7987d278e058
Eugene Kosukhin <kosuha606@gmail.com>
2023-12-27 08:54:24 +0300

    feat: anyof

--- medium/949-anyof.ts
+++ medium/949-anyof.ts
@@ -1,3 +1,4 @@
 type AnyOf<T extends readonly any[]> = T extends Array<0 | '' | undefined | null | false | [] | Record<string, never>> ? false : true
 
 type one = AnyOf<[0, '', false, [], {}, undefined, null]>
+
